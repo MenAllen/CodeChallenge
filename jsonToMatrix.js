0ecb3ceafe6dff8f3ae16cc1b5ce38bd6eb8fcfb
Philippe Joubard <philippe.joubard@sfr.fr>
2023-05-24 18:41:04 +0200

    updated daily test

--- jsonToMatrix.js
+++ jsonToMatrix.js
@@ -1,4 +1,7 @@
 var jsonToMatrix = function(arr) {
+
+  console.log(arr)
+  console.log(typeof arr[0])
   
   let matrixResult = [];
   let listKeys = [];
@@ -8,7 +11,7 @@ var jsonToMatrix = function(arr) {
   if (arr === null) {
     return 'null'
   }
-  if (arr === undefined) {
+   if (arr === undefined) {
     return 'undefined'
   }
   
@@ -17,19 +20,19 @@ var jsonToMatrix = function(arr) {
      for (let i = 0; i< arr[0].length; i++) {
          matrixResult.push(arr[0][i])
       }
-    return matrixResult;
+    console.log('matrixResult Array: ', matrixResult);
   }
 
   // type objet
   if (typeof arr[0] === 'object') {
     
     // objet = {} ?
-    if (Object.keys(arr[0]).length === 0 && arr[0].constructor === Object) {
-      for (let i = 0; i< arr[0].length; i++) {
-         matrixResult.push([]);
-      }
-      return matrixResult;
-    };
+    //if (Object.keys(arr[0]).length === 0 && arr[0].constructor === Object) {
+    //  for (let i = 0; i< arr[0].length; i++) {
+    //     matrixResult.push([]);
+    //  }
+    //  return matrixResult;
+    //};
 
     // c'est un objet classique avec keys et values
     listValuesKeys = [];
@@ -43,10 +46,17 @@ var jsonToMatrix = function(arr) {
       }
     }
     
-    // Filtrer les keys pour éliminer les doublons
-    tab = [...new Set(tab)];
+    // Filtrer les keys pour éliminer les doublons et trier le tableau par o alphabetique
+    tab = [...new Set(tab)].sort((a, b) => { 
+      if (a < b) {
+        return -1;
+      } else {
+        return 1;
+      };    
+    });
     listValuesKeys.push(tab);
     tab = [];
+    console.log(listValuesKeys);
     
     // Renseigner la liste des keys dans resultMatrix
     matrixResult.push(listValuesKeys[0]);
@@ -54,15 +64,24 @@ var jsonToMatrix = function(arr) {
     // Balayer arr pour construire chaque ligne
     for (let j=0; j<arr.length; j++) {
       for (let i=0; i<matrixResult[0].length; i++) {
-        if (arr[j][matrixResult[0][i]]) {
+        if (arr[j][matrixResult[0][i]])  {
           tab.push(arr[j][matrixResult[0][i]]);
-        }
-      }  
+        } else 
+          tab.push("");
+        console.log(tab)
+      }
       matrixResult.push(tab)
       tab=[];
     }
     
 //     console.log('MatrixResult Object: ', matrixResult);
      return matrixResult;    
-   }  
+   }
+//    console.log('End: ', matrixResult);
+    
 };
+
+//console.log(jsonToMatrix([{"b": 1, "a": 2},{"b": 3, "a": 4}]));
+//console.log(jsonToMatrix([{"a": 1, "b": 2},{"c": 3, "d": 4}, {}]))
+//console.log(jsonToMatrix([{}, {}, {},]))
+console.log(jsonToMatrix([{"a": {"b": 1, "c": 2}},{"a": {"b": 3, "d": 4}}]))
